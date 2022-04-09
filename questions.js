b7f7189fa1691cda425bf130af4ebfed4cb13d10
Bernardo Fraga <92233318+beFraga@users.noreply.github.com>
2022-04-09 15:05:49 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,31 @@
+const questions = [{
+    "p": "pergunta 1",
+    "r1": "resposta 1",
+    "r2": "resposta 2",
+    "r3": "resposta 3",
+    "r4": "resposta 4",
+}, {
+    "p": "pergunta 2",
+    "r1": "resposta 1",
+    "r2": "resposta 2",
+    "r3": "resposta 3",
+    "r4": "resposta 4",
+}, {
+    "p": "pergunta 3",
+    "r1": "resposta 1",
+    "r2": "resposta 2",
+    "r3": "resposta 3",
+    "r4": "resposta 4",
+}, {
+    "p": "pergunta 4",
+    "r1": "resposta 1",
+    "r2": "resposta 2",
+    "r3": "resposta 3",
+    "r4": "resposta 4",
+}, {
+    "p": "pergunta 5",
+    "r1": "resposta 1",
+    "r2": "resposta 2",
+    "r3": "resposta 3",
+    "r4": "resposta 4",
+}, ]
